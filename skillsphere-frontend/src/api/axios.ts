ff578f446579f9175c3a0093b943d161944d2b08
Narendra Reddy <mulanarendrareddy@gmail.com>
2026-06-11 23:03:28 +0530

    changes

--- skillsphere-frontend/src/api/axios.ts
+++ skillsphere-frontend/src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useAuthStore } from '@/store/authStore';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://skillsharefronted.vercel.api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://skillsharebackend-j5x4.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
